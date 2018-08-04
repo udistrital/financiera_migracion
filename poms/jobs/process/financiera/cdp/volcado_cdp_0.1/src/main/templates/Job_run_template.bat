311427da13a548002be01cc6dc26f54af88771bb
miguelramirez93 <darckzios@gmail.com>
2018-08-03 20:50:49 -0500

    add auto load job

---
+++ poms/jobs/process/financiera/cdp/volcado_cdp_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.15.jar;../lib/log4j-1.2.16.jar;../lib/ojdbc14.jar;../lib/postgresql-9.4-1201.jdbc41.jar;../lib/talend-oracle-timestamptz.jar;../lib/trove.jar;volcado_cdp_0_1.jar; financiera_migr.volcado_cdp_0_1.volcado_cdp  %* 
---
+++ poms/jobs/process/financiera/secuencia_jobs_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.15.jar;../lib/log4j-1.2.16.jar;../lib/ojdbc14.jar;../lib/postgresql-9.4-1201.jdbc41.jar;../lib/talend-oracle-timestamptz.jar;../lib/trove.jar;secuencia_jobs_0_1.jar;ajuste_cdp_solicitud_0_1.jar;volcado_cdp_0_1.jar; financiera_migr.secuencia_jobs_0_1.secuencia_jobs  --context=Default %* 
